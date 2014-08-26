10d7250d6b269c213c3b5f07267f8e4d37d9fce1
JKair <jkfljy@gmail.com>
2014-08-26 15:03:27 +0800

    文件自己写啊！数据自己读啊！

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 import urllib2
 import re
-
+import MySQLdb
 # 百度软件中心的链接
 base_url = 'http://rj.baidu.com/'
 list_url = base_url + 'soft/lists/%s'
 detail_url = base_url + 'soft/detail/%s.html'
-
 
 # 获取html
 def get_html(url):
@@ -61,26 +60,58 @@ def get_detail_info(detail_url):
         # 此处小坑，百度上部分软件没有官网/介绍图，所以做特殊处理！！！！！
         if _website == [] :
             print '暂无官网'
+            _website[0] =  '暂无官网'
         else:
             print _website[0]
         if _softpic == [] :
             print '暂无图片'
+            _softpic = '暂无图片'
         else:
             print _softpic[0]
-        # 特殊处理完毕！！！！
-
-        # TODO 存到数据库去
+        
+        #数据库操作
+        conn=MySQLdb.connect(host="localhost",user="root",passwd="",db="rjbaidu",charset="utf8",port=3306)
+    
+        cur=conn.cursor()
+        sql="INSERT INTO info(softname,classify,softpic,version,size,downloadlink,website,introduction,whatisnew) VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s')" %(_softname[0],_classify[0],_softpic[0],_version[0],_size[0],_downloadlink[0],_website[0],_introduction[0],_introduction[1])
+        cur.execute(sql)
+        conn.commit()
+        conn.close()
 
     except Exception as e:
         print '有错误：'
         print e
         return
 
+def write_link_in_file():
+    classify=['聊天通讯','输入法','浏览器','下载工具','影视播放','音乐播放','图像编辑','杀毒防护','压缩刻录','系统工具','驱动程序','办公学习','程序开发','影音编辑','手机管理']
+    i = 0
+    for row in classify:
+        file_path = 'E://1/'
+        conn=MySQLdb.connect(host="localhost",user="root",passwd="",db="rjbaidu",charset="utf8",port=3306)
+        cur=conn.cursor()
+        sql="select * from info where classify='"+row+"'"
+        cur.execute(sql)
+        results=cur.fetchall()
+        link=[]
+        file_path = file_path + str(i) + '.txt'
+        i = i + 1
+        for row in results:
+            link.append(row[6])
+        fp=open(file_path,'w')
+        for row in link:
+            fp.write(row)
+            fp.write('\n')
+
+     
+
 
 if __name__ == '__main__':
+    # for i in get_list_arr():
+    #     for j in get_detail_arr(i):
+    #         get_detail_info(j)
 
-    for i in get_list_arr():
-        for j in get_detail_arr(i):
-            get_detail_info(j)
+
+    write_link_in_file()
 
     # get_detail_info(detail_url % 15094)
